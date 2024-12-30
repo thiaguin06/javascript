7f6c092eb35b13f5dc7eafb152cb3203fc2d119d
Thiago Alcantara <167221329+thiaguin06@users.noreply.github.com>
2024-12-30 11:44:50 -0300

    fiz as aulas de js, com varias materia novas, e alguns exercicios tambem

---
+++ aula08/ambiente.js
@@ -0,0 +1,14 @@
+/*console.log('tudo bem') */
+
+/*var c = 1
+while (c <= 1000) {
+    console.log(`passo ${c}`)
+    c++
+}
+ */
+
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 10)
---
+++ aula08/teste.js
@@ -0,0 +1,11 @@
+ /*var c  = 1
+while (c <= 10) {
+    console.log(c) 
+    c++
+} 
+*/
+console.log('vai começas')
+for ( var c= 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('fim')
