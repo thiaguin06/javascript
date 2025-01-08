a1501f54049b933d5dafc7a6574a6dec37775bf5
Thiago Alcantara <167221329+thiaguin06@users.noreply.github.com>
2025-01-07 22:21:04 -0300

    fazendo os estudos de array com vetores

--- aula08/ex17/script.js
+++ aula08/ex17/script.js
@@ -1,1 +1,10 @@
-alert('fala zz')
+function tabuada (){
+    var num = document.getElementById('txtn').value
+    var tab = document.getElementById('seltab')
+    tab.innerHTML = ''
+    for(var c = 1; c <= 10; c++){
+        var item = document.createElement('option')
+        item.text = `${num} x ${c} = ${num * c}`
+        tab.appendChild(item)
+    }
+}
---
+++ aula09/ambiente.js
@@ -0,0 +1,7 @@
+let num = [2, 5, 9, 1]
+num.push(0)
+num.length
+num.sort()
+console.log(num)
+let pos = num.indexOf(9)
+console.log(`o valor 9 está na posição ${pos}`)
