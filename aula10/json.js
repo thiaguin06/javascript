28747d3699a63c5e4d6fc1a306b0880f009393aa
Thiago Alcantara <167221329+thiaguin06@users.noreply.github.com>
2025-01-31 16:25:42 -0300

    estudando json em js

---
+++ aula10/json.js
@@ -0,0 +1,15 @@
+ /*{
+    "nome: joao",
+    "idade: 3 ",                        // aqui ja esta em json agora vou fazer em objeto pra testar
+    "cidade: sao paulo"
+    "habilidades: [falar, comer, dormir]"  
+}  */
+
+     const pessoa = {
+        nome : "thiago",
+        idade : 18,        // aqui esta em objeto
+        cidade : "itacarambi",
+     }  
+     const jsonString = JSON.stringify(pessoa);      // convertando para json. Sempre coloca o S maisculo e o JSON TBM nao esquece nao cabeçao
+     console.log(jsonString);     // lembra do S maisculo
+      
