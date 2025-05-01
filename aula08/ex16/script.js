68fab509037e441478ce9ccf4f8eec1a482e35c2
thiaguin06 <alcantarat219@gamil.com>
2025-05-01 17:16:05 -0300

    Update script.js
    modifiquei

--- aula08/ex16/script.js
+++ aula08/ex16/script.js
@@ -1,8 +1,8 @@
 function contar() {
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
     
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
          res.innerHTML = 'Impossivel contar'
@@ -10,9 +10,9 @@ function contar() {
        
     } else {
         res.innerHTML = 'Contando: <br>'
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
         if (p <= 0) {
             window.alert('Passo inválido!')
             p = 1
